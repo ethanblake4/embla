f6c89ff7bb4410d108ccd31ff8a78189ed452157
ethanblake4 <ethanblake@gmail.com>
2018-07-06 09:53:05 -0700

    star rating, pull to refresh in account

---
+++ lib/widget/star_rating.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+typedef void RatingChangeCallback(double rating);
+
+class StarRating extends StatelessWidget {
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color color;
+  final double starSize;
+
+  StarRating({this.starCount = 5, this.rating = .0, this.onRatingChanged, this.color, this.starSize = 18.0});
+
+  Widget buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = new Icon(
+        Icons.star_border,
+        color: Theme.of(context).buttonColor,
+        size: starSize
+      );
+    }
+    else if (index > rating - 1 && index < rating) {
+      icon = new Icon(
+        Icons.star_half,
+        color: color ?? Theme.of(context).primaryColor,
+        size: starSize,
+      );
+    } else {
+      icon = new Icon(
+        Icons.star,
+        color: color ?? Theme.of(context).primaryColor,
+        size: starSize,
+      );
+    }
+    return new InkResponse(
+      onTap: onRatingChanged == null ? null : () => onRatingChanged(index + 1.0),
+      child: icon,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(children: new List.generate(starCount, (index) => buildStar(context, index)));
+  }
+}
